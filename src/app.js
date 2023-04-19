d3549500bba8f07fba6390d0ee11daacb6be0a79
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-04-19 16:47:39 -0300

    Handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from "express";
 import  productsRouter  from "./routers/products.router.js";
 import  cartsRouter  from "./routers/carts.router.js";
+import __dirname from "./utils.js";
+import handlebars from "express-handlebars";
 
 const app = express();
 app.use(express.json());
+
+// app.use(express.static(__dirname+'/public'));
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', __dirname+'/views');
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index', {name: 'Mabel'});
+});
 
 app.use('/api/products/', productsRouter);
 app.use('/api/carts/', cartsRouter);
