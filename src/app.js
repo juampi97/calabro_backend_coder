4a5e94919bc46152569c09fc22b8be079a7c0959
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-04-26 11:22:40 -0300

    server

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 import express from "express";
 import __dirname from "./utils.js";
 import handlebars from "express-handlebars";
-//import productsRouter from "./routers/products.router.js";
-//import cartsRouter from "./routers/carts.router.js";
+import productsRouter from "./routers/products.router.js";
+import cartsRouter from "./routers/carts.router.js";
 import viewsRouter from "./routers/views.router.js";
 import { Server } from "socket.io";
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 const httpServer = app.listen(8080, () =>
   console.log("Servidor escuchando en el puerto 8080")
 );
 
-const socketServer = new Server(httpServer);
+const io = new Server(httpServer);
 
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname+"/views");
@@ -22,13 +22,13 @@ app.set("view engine", "handlebars");
 app.use(express.static(__dirname+"/public"));
 
 app.use("/", viewsRouter);
-//app.use("/api/products/", productsRouter);
-//app.use("/api/carts/", cartsRouter);
+app.use("/api/products/", productsRouter);
+app.use("/api/carts/", cartsRouter);
 
-socketServer.on("connection", (socket) => {
+io.on("connection", (socket) => {
   console.log("Nuevo cliente conectado!");
   socket.on("new-product", (data) => {
     console.log(data);
-    socketServer.emit("new-product", data);
+    io.emit("new-product", data);
   });
 });
