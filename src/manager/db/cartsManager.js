ac884a9003945407ac1f3a4167956f9b6f4db77f
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-05-24 15:21:54 -0300

    Cart

--- src/manager/db/cartsManager.js
+++ src/manager/db/cartsManager.js
@@ -16,53 +16,60 @@ class CartsManger {
   };
 
   getCarts = async () => {
-    try{
+    try {
       let carts = await cartModel.find();
       return carts;
-    } catch(err) {
+    } catch (err) {
       console.log("Cannot get carts with mongoose: " + err);
     }
   };
 
   getCartById = async (id) => {
-    try{
+    try {
       // let cart = await cartModel.findOne({_id: id}).populate("products.product");
-      let cart = await cartModel.findOne({_id: id});
+      let cart = await cartModel.findOne({ _id: id });
       return cart;
-    } catch(err) {
+    } catch (err) {
       return "406";
     }
-
   };
 
   addProductToCart = async (cid, pid) => {
-    try{
-      let product = await productModel.findOne({_id: pid});
-      let cart = await cartModel.findOne({_id: cid});
-      
-      cart.products.push( {product: pid} );
-      const result = await cartModel.updateOne({_id: cid}, cart);
-
-      /*if(cart.products.length == 0) {
-        let item = {id:product._id, cantidad:1};
-        cart.products.push(item);
+    try {
+      let product = await productModel.findOne({ _id: pid });
+      let cart = await cartModel.findOne({ _id: cid });
+      console.log("Mi producto");
+      console.log(cart.products[0].id.toString());
+      if (cart.products.length == 0) {
+        cart.products.push({ id: pid, qty: 1 });
+        const result = await cartModel.updateOne(
+          { _id: cid },
+          { products: cart.products }
+        );
       } else {
         let found = false;
-        for(let i = 0; i < cart.products.length; i++) {
-          if(cart.products[i].id == pid) {
-            cart.products[i].cantidad++;
+        console.log("Prods carrito");
+        for (let i = 0; i < cart.products.length; i++) {
+          console.log(cart.products[i].id.toString());
+          if (cart.products[i].id.toString() == pid) {
+            cart.products[i].qty++;
             found = true;
+            break;
           }
         }
-        if(!found) {
-          let item = {id:product._id, cantidad:1};
-          cart.products.push(item);
+        console.log(found);
+        if (!found) {
+          cart.products.push({ id: pid, qty: 1 });
         }
-      }*/
-      console.log(result);
-      return "200"
-    } catch(err) {
-      console.log("Cannot add product to cart with mongoose: " + err);
+        const result = await cartModel.updateOne(
+          { _id: cid },
+          { products: cart.products }
+        );
+      }
+      
+      return "200";
+    } catch (err) {
+      console.log("Cannot get carts with mongoose: " + err);
       return "406";
     }
   };
--- src/model/carts.model.js
+++ src/model/carts.model.js
@@ -3,23 +3,22 @@ import mongoose from "mongoose";
 const cartCollection = "carts";
 
 const cartSchema = new mongoose.Schema({
-    products: { 
-        type: [
-            {
-                product: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "products"
-                }    
-            }
-        ],
-        default: [] 
-    }
+  products: {
+    type: [
+      {
+        // id: { type: mongoose.Schema.Types.ObjectId, ref: "products" },
+        id: { type: String },
+        qty: { type: Number },
+      },
+    ],
+    default: [],
+  },
 });
 
-cartSchema.pre('findOne', function() {
-    this.populate('products.product');
-});
+// cartSchema.pre("findOne", function () {
+//   this.populate("products.product");
+// });
 
 const cartModel = mongoose.model(cartCollection, cartSchema);
 
-export default cartModel
+export default cartModel;
